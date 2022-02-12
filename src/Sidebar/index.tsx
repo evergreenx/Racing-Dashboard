56fb7da2c97c5b7c70dd2e384150d30b71ada193
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-02-13 00:49:12 +0100

    jj

--- src/Sidebar/index.tsx
+++ src/Sidebar/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+
 import Logo from "../image/logo.svg";
 import { BiHomeAlt } from "react-icons/Bi";
+
+
+import { BiChevronDown } from "react-icons/Bi";
+
+
 import "./Sidear.css";
+
+import ProfileImage from "../image/profile.png";
 
 export default function index() {
     return (
-        <div className="w-40 sidebar h-screen border-r-2 flex flex-col justify-between border-gray-300 p-10">
+        <div className="w-40 sidebar h-screen border-r flex flex-col justify-between border-gray-300 p-10">
             <div className="logo-container">
 
                 <img src={Logo} alt="logo" />
@@ -22,40 +29,64 @@ export default function index() {
                 </div>
 
 
-               
+
             </div>
 
-            <div className="scheduled-races-container flex flex-col ">
+            <div className="scheduled-races-container flex flex-col space-y-6 ">
+
+<h2 className="text-[#3629B7]" >
+Scheduled Races
+</h2>
+
+                <div className="race flex items-center space-x-5 ">
+                    <p className="w-4 h-4 border border-[#3F7EF8] rounded-full">
+                    </p>
+                    <p className="text-sm font-normal">
+                        MotoGP 2022
+                    </p>
+                </div>
 
 
-<div className="race flex items-center space-x-5 ">
-    <p className="w-4 h-4 border border-[#3F7EF8] rounded-full">
-    </p>
-    <p className="text-sm font-normal">
-        MotoGP 2022
-    </p>
-</div>
+                <div className="race flex items-center space-x-5 ">
+                    <p className="w-4 h-4 border border-[#FA6C6A] rounded-full">
+                    </p>
+                    <p className="text-sm font-normal">
+                        Dynamics 22
+                    </p>
+                </div>
 
-
-<div className="race flex items-center space-x-5 ">
-    <p className="w-4 h-4 border border-[#FA6C6A] rounded-full">
-    </p>
-    <p className="text-sm font-normal">
-        Dynamics 22
-    </p>
-</div>
-
-<div className="race flex items-center space-x-5 ">
-    <p className="w-4 h-4 border border-[#27C1B3] rounded-full">
-    </p>
-    <p className="text-sm font-normal">
-        Olympics
-    </p>
-</div>
+                <div className="race flex items-center space-x-5 ">
+                    <p className="w-4 h-4 border border-[#27C1B3] rounded-full">
+                    </p>
+                    <p className="text-sm font-normal">
+                        Olympics
+                    </p>
+                </div>
 
 
 
-</div>
+            </div>
+
+            <div className="logout-user p-3 bg-[#F9FAFF] rounded-lg flex items-center space-x-5">
+
+
+
+                <div className="profile-image flex items-center space-x-4">
+
+                    <img src={ProfileImage} alt="profile" />
+
+                    <h2 className="text-[#591AED] font-semibold">
+
+                        Killua
+                    </h2>
+                </div>
+
+                <div className="next-icon">
+                    <BiChevronDown />
+                </div>
+
+
+            </div>
         </div>
     );
 }
