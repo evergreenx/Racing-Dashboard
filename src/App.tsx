2786efd8ce375464019bc4c9884edc15b5fd3916
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-02-16 13:03:36 +0100

    optimized for desktop only

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import Sidebar from './Sidebar'
 import Topbar from './Topbar'
+import Logo from './image/logo.svg'
 
 import './App.css'
 import MainContent from './MainContent'
@@ -10,13 +10,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="App ">
 
 
 
-      <Sidebar />
 
-      <MainContent />
+
+      <div className="
+hidden lg:block">
+
+        <Sidebar />
+        <MainContent />
+
+      </div>
+
+
+      <div className="block lg:hidden">
+
+
+        <h2>
+          {/* best view on desktop */}
+
+          <div className="flex justify-center items-center w-full h-screen flex-col">
+            <div className="">
+              <img src={Logo} className="App-logo animate-pulse" alt="logo" width={200} />
+
+              </div>
+
+              <h2 className='text-[#3629B7] font-bold text-xl'>
+
+                Dashboard is optimized for desktop only
+              </h2>
+          </div>
+
+        </h2>
+      </div>
 
     </div>
   )
